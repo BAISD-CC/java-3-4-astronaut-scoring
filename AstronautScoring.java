5e4ada25b04b68c33cc6b166ddcaf1dccfac1de9
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 17:34:03 +0000

    Create assignment

---
+++ AstronautScoring.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class AstronautScoring {
+
+    public static void main(String[] args) {
+        // Variables for the astronauts.
+
+        // Variables for the places.
+
+        // Variables for the scores.
+
+        // Create a Scanner object for keyboard input.
+
+        // Display introduction message.
+
+        // Get the first astronaut's information.
+
+        // Get the second astronaut's information. 
+
+        // Get the third astronaut's data.
+
+        // Close the keyboard scanner
+
+        // Determine the first place astronaut.
+
+        // Determine the second place astronaut.
+
+        // Determine the third place astronaut.
+
+        // Display the results.
+
+        // Close the application.
+        
+    }
+
+}
